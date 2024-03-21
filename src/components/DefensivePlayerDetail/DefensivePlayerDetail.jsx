c6247b9b25d46ab89d6662ae54e0d47225e7f4fd
Brandon Taylor <156858984+brandontaylor1@users.noreply.github.com>
2024-03-20 21:58:30 -0500

    modify PlayerPage, create both OffensivePlayerDetail and DefensivePlayerDetail

---
+++ src/components/DefensivePlayerDetail/DefensivePlayerDetail.jsx
@@ -0,0 +1,197 @@
+import React, { useState, useEffect }from "react";
+import { useParams } from "react-router-dom";
+import { Card, CardBody, CardTitle, CardText, CardLink, ListGroup, ListGroupItem } from 'reactstrap';
+
+
+const DefensivePlayerDetail = () => {
+    const [sortedPlayers, setDefensivePlayers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);   
+
+    const { teamId } = useParams();
+
+    useEffect(() => {
+
+        const fetchPlayers = async () => {
+            try {
+                const url = `https://nfl-api-data.p.rapidapi.com/nfl-player-listing/v1/data?id=${teamId}`;
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': 'ba5bba66e7msh4d67f7a5909ce85p1001bdjsnefb21fbd6220',
+                        'X-RapidAPI-Host': 'nfl-api-data.p.rapidapi.com'
+                    }
+                };
+
+            const response = await fetch(url, options);
+            const result = await response.json();
+            const positionOrder = {
+                QB: 1,
+                RB: 2,
+                FB: 3,
+                WR: 4,
+                TE: 5,
+                C: 6,
+                G: 7,
+                T: 8,
+                NT: 9,
+                DT: 10,
+                DE: 11,
+                ILB: 12,
+                OLB: 13,
+                CB: 14, 
+                S: 15,
+                K: 16,
+                P: 17,
+                LS: 18, 
+            }
+
+            const sortedPlayers = 
+                result.athletes[1].items.sort((a, b) => {
+                const positionA = a.position.abbreviation;
+                const positionB = b.position.abbreviation;
+                const orderA = positionOrder[positionA] || Infinity;
+                const orderB = positionOrder[positionB] || Infinity;
+
+                return orderA - orderB; 
+            })
+
+            setDefensivePlayers(sortedPlayers);
+            setLoading(false);
+
+            } catch (error) {
+                console.log(error);
+                setError(error.message)
+                setLoading(false)
+            }
+        }
+        fetchPlayers(); 
+    }, [teamId])
+
+    if(loading) return <h1>Loading...</h1> 
+    if(error) return <h1>Something went wrong! {error}</h1>
+
+    return (
+        <section className="defensive-players">
+            <h2 style={{
+                fontSize: '2vw',
+                fontWeight: 'bold',
+            }}>Defensive Players</h2>
+            <ul>
+
+                {sortedPlayers.map((player, index) => {
+                    const playerId = player.id
+                    const playerName = player.displayName;
+                    const playerPosition = player.position.abbreviation;
+                    const playerNumber = player.jersey;
+                    const playerHeight = player.displayHeight;
+                    const playerWeight = player.displayWeight;
+                    const playerImage = player.headshot.href;
+                    const playerImageAlt = player.headshot.alt
+                    const playerDOB = player.dateOfBirth;
+                    const playerCollege = player.college ? player.college.name : "Unknown";
+                    const formatedDOB = new Date(playerDOB).toLocaleDateString('en-US', {month: 'short', day: '2-digit', year: 'numeric'});
+                    const age = player.age;
+                    return (
+                            <Card key={playerId}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    position: 'relative',
+                                    width: '18rem',
+                                    backgroundColor: 'white',
+                                    border: '1px solid lightgray',
+                                    color: 'black',
+                                    borderRadius: '20px',
+                                    margin: '10px',
+                                    padding: '10px',
+                                    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                                }}
+                                
+                                >
+                                
+                                <img className="Card" src={playerImage} alt={playerImageAlt}/>
+                                <CardBody>
+                                    <CardTitle tag="h2">
+                                        <h2 style={{
+                                            fontweight: 'bold',
+                                            fontSize: '1.25vw',
+                                            marginBottom: '10px'
+                                        }}>{playerName}</h2>
+                                    </CardTitle>
+                                    <CardText style={{
+                                        textAlign: 'center',  
+                                        fontweight: 'bold',
+                                        fontSize: '1vw',
+                                        marginTop: '-20px'
+                                    }}><span style={{
+                                    position: 'absolute',
+                                    top: '5px',
+                                    left: '5px',
+                                    color: 'black',
+                                    opacity: '0.1',
+                                    fontWeight: 'bold',
+                                    fontSize: '3vw'     
+                                }}>{playerPosition}</span>  
+                                    <span style={{
+                                        position: 'absolute',
+                                        top: '5px',
+                                        right: '5px',
+                                        color: 'black',
+                                        opacity: '0.1',
+                                        fontWeight: 'bold',
+                                        fontSize: '3vw'
+                                    }}>{playerNumber}</span>
+                                    </CardText>
+                                </CardBody>
+                                <ListGroup flush>
+                                    <ListGroupItem>
+                                        Ht: {playerHeight}   |   Wt: {playerWeight} | Spd: 99
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        College: {playerCollege}  |  Exp: {player.experience.years}
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        DOB: {formatedDOB}   |   Age: {age}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                        Birthplace: {player.birthPlace.city}, {player.birthPlace.state}
+                                </ListGroupItem>
+                            </ListGroup>
+                            <CardBody>
+                                <CardLink href="#">
+                                <button style={{
+                                    backgroundColor: 'black',
+                                    color: 'white',
+                                    padding: '10px',
+                                    borderRadius: '10px',
+                                    border: 'none',
+                                    cursor: 'pointer',
+                                    margin: '5px',
+                                    width: '100px'
+                                }}>Stats</button>
+                                </CardLink>
+                                <CardLink href="#">
+                                <button style={{
+                                    backgroundColor: 'black',
+                                    color: 'white',
+                                    padding: '10px',
+                                    borderRadius: '10px',
+                                    border: 'none',
+                                    cursor: 'pointer',
+                                    margin: '5px',
+                                    width: '100px'
+                                }}>News</button>
+                                </CardLink>
+                            </CardBody>
+                        </Card>
+                        )
+                    })}
+            </ul>
+        </section>
+    )
+}
+
+export default DefensivePlayerDetail;
